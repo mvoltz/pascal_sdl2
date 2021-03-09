7d8a995bca09e0c82f29b31ecf71fccd77e72d43
mvoltz <mark.voltz@gmail.com>
2021-03-08 23:50:21 -0600

    rectangle file sus

---
+++ 3.ball_using_SDL_image.pas
@@ -0,0 +1,52 @@
+program ball_using_SDL_image;
+
+{ 
+    https://www.freepascal-meets-sdl.net/window-and-renderer/
+    https://www.freepascal-meets-sdl.net/surfaces-and-textures/ 
+    https://www.freepascal-meets-sdl.net/loading-and-rendering-a-bitmap-file/
+    https://www.freepascal-meets-sdl.net/chapter-4-imagestextures/
+}
+
+uses 
+    SDL2, SDL2_image;
+
+var
+    sdlWindow1: PSDL_Window;
+    sdlRenderer: PSDL_Renderer;
+    sdlTexture1: PSDL_Texture;
+    window_width: SInt32;
+    window_height: SInt32;
+
+begin
+
+    window_width := 800;
+    window_height := 450;
+
+
+    if SDL_Init(SDL_INIT_VIDEO) < 0 then Halt;
+
+
+    sdlWindow1 := SDL_CreateWindow('Window1', SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, window_width, window_height, SDL_WINDOW_SHOWN);
+
+    sdlRenderer := SDL_CreateRenderer(sdlWindow1, -1, 0);
+    if sdlRenderer = nil then Halt;
+
+    // this is where we use SDL_image
+
+    sdlTexture1 := IMG_LoadTexture( sdlRenderer, 'art/example_ball.png' );
+    if sdlTexture1 = nil then Halt;
+
+
+    SDL_RenderCopy(sdlRenderer, sdlTexture1, nil, nil);
+    SDL_RenderPresent(sdlRenderer);
+    SDL_Delay(2000);
+
+
+    SDL_DestroyTexture(sdlTexture1);
+    SDL_DestroyRenderer(sdlRenderer);
+    SDL_DestroyWindow(sdlWindow1);
+
+
+    SDL_Quit;
+
+end.
---
+++ 4.rectangles.pas
@@ -0,0 +1,66 @@
+program rectangles;
+
+{ 
+    https://www.freepascal-meets-sdl.net/window-and-renderer/
+    https://www.freepascal-meets-sdl.net/surfaces-and-textures/ 
+    https://www.freepascal-meets-sdl.net/loading-and-rendering-a-bitmap-file/
+    https://www.freepascal-meets-sdl.net/chapter-4-imagestextures/
+    https://www.freepascal-meets-sdl.net/rectangles-movement-and-scaling/
+}
+
+uses 
+    SDL2, SDL2_image;
+
+var
+    sdlWindow1: PSDL_Window;
+    sdlRenderer: PSDL_Renderer;
+    sdlSurface1: PSDL_Surface;
+    sdlTexture1: PSDL_Texture;
+    sdlRectangle: TSDL_Rect;
+    window_width: SInt32;
+    window_height: SInt32;
+
+begin
+
+    window_width := 800;
+    window_height := 450;
+
+
+    if SDL_Init(SDL_INIT_VIDEO) < 0 then Halt;
+
+
+    if SDL_CreateWindowAndRenderer(window_width, window_height, SDL_WINDOW_SHOWN, @sdlWindow1, @sdlRenderer) <> 0 then Halt;
+
+    // set scaling quality
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, 'nearest');
+
+    // create surface from file
+    sdlSurface1 := SDL_LoadBMP('art/example_ball.bmp');
+    if sdlSurface1 = nil then Halt;
+
+    // load image file
+    sdlTexture1 := SDL_CreateTextureFromSurface(sdlRenderer, sdlSurface1);
+    if sdlTexture1 = nil then Halt;
+
+    // describe rectangle
+    sdlRectangle.x := 12;
+    sdlRectangle.y := 25;
+    sdlRectangle.w := 178;
+    sdlRectangle.h := 116;
+
+
+    SDL_RenderCopy(sdlRenderer, sdlTexture1, @sdlRectangle, nil);
+    SDL_RenderCopy(sdlRenderer, sdlTexture1, nil, @sdlRectangle);
+    SDL_RenderPresent(sdlRenderer);
+    SDL_Delay(2000);
+
+
+    SDL_DestroyTexture(sdlTexture1);
+    SDL_FreeSurface(sdlSurface1);
+    SDL_DestroyRenderer(sdlRenderer);
+    SDL_DestroyWindow(sdlWindow1);
+
+
+    SDL_Quit;
+
+end.
